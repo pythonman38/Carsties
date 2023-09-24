45bc20e409f793eca6e24fcd759d54fac1a995d4
Michael Walker <pythonk@email.phoenix.edu>
2023-09-24 18:40:53 -0400

    Step 5 - Adding a Gateway Service
    Added a gateway service (Reverse Proxy) to allow the individual microservices to communicate directly with the client-side app (website) as well as an indirect link to each other. Also to prepare for dockerization in the next commit, I added the remaining consumers to both the Auction and Search microservices. Also added a new client to the Identity microservice to return data to the website and not just Postman.

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    private readonly AuctionDbContext dbContext;
+
+    public AuctionFinishedConsumer(AuctionDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--> Consuming auction finished!");
+
+        var auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice ? Entities.Status.Finished : Entities.Status.ReserveNotMet;
+
+        await dbContext.SaveChangesAsync();
+    }
+}
---
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,29 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    private readonly AuctionDbContext dbContext;
+
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed!");
+
+        var auction = await dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (auction.CurrentHighBid == null || context.Message.BidStatus.Contains("Accepted")
+            && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
